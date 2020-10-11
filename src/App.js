918630195ed211e7cbfc227345805d9ae433c231
swimjiy <swimjiy@gmail.com>
2020-10-11 18:04:17 +0900

    [CHORE] Set React env

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>header</header>
+      Hello World!
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
