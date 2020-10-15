c5125428ac8efd5c243a7143f716f68e98c959c3
Swimjiy <swimjiy@gmail.com>
2020-10-15 12:20:19 +0900

    [CHORE] Install eslint & Fix lint error (#5)

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb',
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['js', 'jsx'] }],
+    'no-console': ['off'],
+    'react/jsx-props-no-spreading': ['warn'],
+  },
+};
