cd80449ecac7e04f027fffe0c5300fdf68217be8
Jiyoon Hur <jiyoon1156@gmail.com>
2020-10-23 16:02:08 +0900

    [FEAT] make pages/Home base

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import Button from '../components/Button';
+import React from "react";
+import Button from "../components/Button";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Home = () => (
-  <>
-    Home
-    <Button>helloWorld</Button>
-  </>
+  <Group>
+    <h2>mkdev 심리테스트</h2>
+    <img src={require("../imgs/tmp.png")} alt="not found" />
+    <b>
+      이것은 설명..이것은 설명...
+      <br />
+      이것은 설명..이것은 설명...
+      <br />
+      이것은 설명..이것은 설명...
+    </b>
+    <Link to="/question">
+      <Button>시작하기</Button>
+    </Link>
+    <b>
+      참여자수 <br /> 1900명
+    </b>
+  </Group>
 );
+
+const Group = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default Home;
