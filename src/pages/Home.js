0680d23d46eebed052fb1bd44e82603501c13ae9
jiyoonhur <jiyoon1156@gmail.com>
2020-10-24 01:07:56 +0900

    [FIX]: eslint, order of modules

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Button from "../components/Button";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import Button from '../components/Button';
 
 const Home = () => (
   <Group>
     <h2>mkdev 심리테스트</h2>
-    <img src={require("../imgs/tmp.png")} alt="not found" />
+    <img src="../imgs/tmp.png" alt="not found" />
     <b>
       이것은 설명..이것은 설명...
       <br />
@@ -18,7 +18,11 @@ const Home = () => (
       <Button>시작하기</Button>
     </Link>
     <b>
-      참여자수 <br /> 1900명
+      참여자수
+      {' '}
+      <br />
+      {' '}
+      1900명
     </b>
   </Group>
 );
