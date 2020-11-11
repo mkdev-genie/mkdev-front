a41a9d7164c6d0268b374f0a6f7d35922269aaec
Jiyoon Hur <jiyoon1156@gmail.com>
2020-11-11 13:26:29 +0900

    [FEAT] Add rounded-text to components

---
+++ src/components/RoundedText.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const RoundedText = ({ children }) => <StyledText>{children}</StyledText>;
+
+RoundedText.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const StyledText = styled.div`
+  border: solid;
+  border-radius: 15px;
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  padding: 0.3rem;
+  color: #e2f063;
+`;
+
+export default RoundedText;
