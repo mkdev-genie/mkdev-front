695d88800e75a7cc54880fc2648c0516e4cd41fc
Jiyoon Hur <jiyoon1156@gmail.com>
2020-11-11 13:26:00 +0900

    [FEAT] Add sub-title to components

---
+++ src/components/SubTitle.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const SubTitle = ({ children }) => (
+  <StyledTitle>
+    &#123;
+    {children}
+    &#125;
+  </StyledTitle>
+);
+
+SubTitle.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const StyledTitle = styled.div`
+  position: flex;
+  width: 217px;
+  height: 36px;
+  left: 80px;
+  top: 99px;
+
+  font-family: Noto Sans HK;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 35px;
+  text-align: center;
+
+  color: #553ff5;
+`;
+
+export default SubTitle;
