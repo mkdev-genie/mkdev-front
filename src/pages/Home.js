0efba85b99412fb79c1e4a72f36c277382786292
Jiyoon Hur <jiyoon1156@gmail.com>
2020-11-11 15:06:56 +0900

    [FEAT] Add share button && image tag

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,18 +5,25 @@ import Button from '../components/Button';
 import MainTitle from '../components/MainTitle';
 import SubTitle from '../components/SubTitle';
 import RoundedText from '../components/RoundedText';
+import ShareButton from '../components/ShareButton';
+import tmp from '../imgs/tmp.png';
 
 const Home = () => (
   <Group>
-    <RoundedText>project.mkdev</RoundedText>
+    <RoundedText>project. mkdev</RoundedText>
     <MainTitle>개발 포지션 테스트</MainTitle>
     <Intro>개발에도 MBTI가 있다!</Intro>
     <Intro>나에게 가장 어울리는 개발 포지션은?</Intro>
-    <img src="../imgs/tmp.jpeg" alt=" " />
+    <Image>그림</Image>
     <Link to="/question">
       <Button>개발 성향 알아보기</Button>
     </Link>
     <SubTitle>공유하기</SubTitle>
+    <Share>
+      <ShareButton>kakao</ShareButton>
+      <ShareButton>facebook</ShareButton>
+      <ShareButton>link</ShareButton>
+    </Share>
   </Group>
 );
 
@@ -31,6 +38,19 @@ const Group = styled.div`
 const Intro = styled.div`
   display: flex;
   color: white;
+`;
+
+const Share = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Image = styled.div`
+  background-image: url(${tmp});
+  width: 335px;
+  height: 200px;
+  margin-top: 10px;
+  margin-bottom: 10px;
 `;
 
 export default Home;
