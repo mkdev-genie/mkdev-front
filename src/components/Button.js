07a513101437e71dd3d33ef27338eb664cae9df5
Jiyoon Hur <jiyoon1156@gmail.com>
2020-11-11 13:28:19 +0900

    [FIX] Styled components for buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,16 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const StyledButton = styled.button`
-  color: red;
-  border: none;
-  background-color: gray;
-`;
-
 const Button = ({ children }) => <StyledButton>{children}</StyledButton>;
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
+const StyledButton = styled.button`
+  background: #e2f063;
+  border: none;
+  border-radius: 10px;
+  padding: 0.5rem;
+  font-family: Noto Sans HK;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 15px;
+`;
 
 export default Button;
