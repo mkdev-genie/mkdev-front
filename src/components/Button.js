81a7c8aea76ca65b9300547943d1717416d3848f
jiyoonhur <jiyoon1156@gmail.com>
2020-11-13 16:29:53 +0900

    [FEAT] Add onClick prop to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Button = ({ children }) => <StyledButton>{children}</StyledButton>;
+const Button = ({ onClick, children }) => (
+  <StyledButton onClick={onClick}>{children}</StyledButton>
+);
 
 Button.propTypes = {
+  onClick: PropTypes.node.isRequired,
   children: PropTypes.node.isRequired,
 };
 
