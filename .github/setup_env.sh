c75e04b2d45ad66241596067a39833be3950c61f
swimjiy <swimjiy@gmail.com>
2021-02-18 00:13:01 +0900

    Fix bad substitution error

--- .github/setup_env.sh
+++ .github/setup_env.sh
@@ -1,2 +1,2 @@
-echo REACT_APP_KAKAO_KEY=${{ secrets.REACT_APP_KAKAO_KEY }} >> .env
-echo REACT_FETCH_URL=${{ secrets.REACT_FETCH_URL }} >> .env
+echo REACT_APP_KAKAO_KEY=secrets.REACT_APP_KAKAO_KEY >> .env
+echo REACT_FETCH_URL=secrets.REACT_FETCH_URL >> .env
